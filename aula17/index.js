8e9f7e2ea1feb168bfe56fb87bb2323f98141570
Denize Sanches da Paixão <rosivaldo.correia@terra.com.br>
2022-06-27 22:32:21 -0300

    Aula 17

---
+++ aula17/index.js
@@ -0,0 +1,16 @@
+function soma (x, y){
+    return resultado = x + y ;
+}
+console. log(soma(2, 1));
+console. log(soma(2, 2));
+console. log(soma(2, 3));
+console. log(soma(2, 4));
+
+
+
+// function sudacao(nome) {
+//     return `Bom dia ${nome}!`
+// }
+
+// const variavel = sudacao('Denize');
+// console.log(variavel);
